976980f87a830508a7daff43e461b52625858391
greiver179 <te_hung@hotmail.com>
2018-12-12 00:13:31 +0100

    Initial commit

---
+++ src/main/java/com/springboottutorial/springboot2tutorial/Springboot2tutorialApplication.java
@@ -0,0 +1,12 @@
+package com.springboottutorial.springboot2tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2tutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2tutorialApplication.class, args);
+	}
+}
